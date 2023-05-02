39370609e580291f882407b20162b1f1123e556b
Veronica-rhody-219 <ngocdiep219@gmail.com>
2023-05-03 05:46:06 +0700

    feat: update all files and folders

--- chatgpt.py
+++ chatgpt.py
@@ -2,12 +2,13 @@ from transformers import pipeline
 
 generator = pipeline('text-generation', model='EleutherAI/gpt-neo-2.7B')
 
+
 while True:
     # Prompt the user for input
     prompt = input("You: ")
 
     # Generate a response to the user input
-    generated_text = generator(prompt, max_length=100, num_return_sequences=1)
+    generated_text = generator(prompt, max_length=50, do_sample = True, temperature=0.9)
 
     # Extract the generated text from the response object
     response = generated_text[0]['generated_text'].strip()
